a46fe1f037ed38f5be624cb2b6ff6db5499bdcd1
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-07-13 23:49:58 +0200

    new

--- datasets/net_extract.py
+++ datasets/net_extract.py
@@ -9,8 +9,8 @@ df_net = pd.read_csv("integratedNet_edges.csv")					# reload csv
 # estract and save in csv PPI, MET and MET+PPI networks
 df_net["IsPhysical"] = df_net.apply(lambda row: True if row.link == "physical" or row.link == "both" else False, axis=1)
 df_net["IsMetabolic"] = df_net.apply(lambda row: True if row.link == "metabolic" or row.link == "both" else False, axis=1)
-df_net.loc[df_net["IsMetabolic"]].drop(columns=['IsPhysical', 'IsMetabolic', 'link']).to_csv("met_edges.csv", index=False)
-df_net.loc[df_net["IsPhysical"]].drop(columns=['IsPhysical', 'IsMetabolic','link']).to_csv("ppi_edges.csv", index=False)
-df_net.drop(columns=['IsPhysical', 'IsMetabolic', 'link']).to_csv("met+ppi_edges.csv")
+df_net.loc[df_net["IsMetabolic"]].drop(columns=['IsPhysical', 'IsMetabolic', 'link']).to_csv("met_edges.csv")
+df_net.loc[df_net["IsPhysical"]].drop(columns=['IsPhysical', 'IsMetabolic','link']).to_csv("ppi_edges.csv")
+#df_net.drop(columns=['IsPhysical', 'IsMetabolic', 'link']).to_csv("met+ppi_edges.csv")
 print(df_net)
 
