0886be7dc40d9226c05cf6f28beaab37cf5cbdcc
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-11-04 15:59:33 +0100

    scorepred.py

---
+++ scorepred.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import random
+
+import argparse
+parser = argparse.ArgumentParser(description='score and predictions')
+parser.add_argument('-d', "--datadir", dest='datadir', metavar='<data-dir>', type=str, help='data directory (default KIDNEY)', default='KIDNEY', required=False)
+args = parser.parse_args()
+
+x = pd.read_csv(f'{args.datadir}/x.csv', index_col=0)
+targets = pd.read_csv(f'{args.datadir}/y.csv', index_col=0)
+
+import lightgbm as  lgb
+from sklearn.svm import SVR
+from sklearn.model_selection import KFold, train_test_split
+from tqdm import tqdm
+from sklearn.metrics import *
+seed = 1
+random.seed(seed)
+np.random.seed(seed)
+nfolds = 5
+kf = KFold(n_splits=nfolds, shuffle=True, random_state=seed)
+regressor_args = {'random_state' : seed}
+X = x.to_numpy()
+y = targets.to_numpy()
+genelist = targets.index.values
+columns = list(targets.columns)
+result = pd.DataFrame()
+for fold, (train_idx, test_idx) in enumerate(tqdm(kf.split(np.arange(len(X)), y), total=kf.get_n_splits(), desc=f"{nfolds}-fold")):
+  train_x, train_y, test_x, test_y, genes = X[train_idx], y[train_idx], X[test_idx], y[test_idx], genelist[test_idx]
+  data = {'name' : list(genes)}
+  for i in range(len(y[0])):
+      prediction = lgb.LGBMRegressor(**regressor_args).fit(train_x, train_y[:,i]).predict(test_x)
+      data[f'pred_{columns[i]}'] = prediction
+      data[f'score_{columns[i]}'] = list(test_y[:,i])
+  result = result.append(pd.DataFrame(data))
+
+print(result)
+result.set_index('name').to_csv(f'{args.datadir}/scorepred.csv')
